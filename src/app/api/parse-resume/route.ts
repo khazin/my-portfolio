de5f7b5f9eb4a06bf5424812c06c7a1f408b484b
khazin <khazin316@gmail.com>
2025-06-19 17:36:30 +0800

    Fix: add type to callback error parameter

--- src/app/api/parse-resume/route.ts
+++ src/app/api/parse-resume/route.ts
@@ -25,12 +25,13 @@ export async function POST(req: NextRequest) {
   try {
     const form = new formidable.IncomingForm();
 
-    const files: FilesType = await new Promise((resolve, reject) => {
-      form.parse(req as unknown as IncomingMessage, (_err, _fields, files) => {
-        if (_err) reject(_err);
-        else resolve(files);
-      });
-    });
+ const files: FilesType = await new Promise((resolve, reject) => {
+  form.parse(req as unknown as IncomingMessage, (err: any, _fields, files) => {
+    if (err) reject(err);
+    else resolve(files);
+  });
+});
+
 
     const file = Array.isArray(files.resume)
       ? files.resume[0]?.filepath
